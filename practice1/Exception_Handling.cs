18a6e5e7b43bede5d2324ae7918133fe675bc0fb
Malek Mansour <138655005+MalekMansour@users.noreply.github.com>
2024-03-01 15:06:30 -0700

    Update Exception_Handling.cs

--- practice1/Exception_Handling.cs
+++ practice1/Exception_Handling.cs
@@ -1,1 +1,39 @@
+using System;
+public class Circle
+{
+    private double Radius;
 
+    public void SetRadius(double radius) => Radius = radius > 0 ? radius : throw new InvalidRadiusException(radius);
+
+    public override string ToString() => Radius > 0 ? $"Circle with Radius: {Radius}" : throw new InvalidRadiusException(Radius);
+}
+
+public class InvalidRadiusException : Exception
+{
+    public InvalidRadiusException(double radius) : base(radius > 0 ? $"Invalid Radius: {radius}" : $"Invalid Radius: {radius}. Radius must be greater than 0.") { }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        PrintValidCircle(6);
+        PrintValidCircle(-2);
+        PrintValidCircle(0);
+        Console.ReadKey();
+    }
+
+    static void PrintValidCircle(double radius)
+    {
+        try
+        {
+            var circle = new Circle();
+            circle.SetRadius(radius);
+            Console.WriteLine(circle);
+        }
+        catch (InvalidRadiusException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
