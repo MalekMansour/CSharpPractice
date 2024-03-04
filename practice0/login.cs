e71c39edfb3321d9531c2fca6cd2a8aa76c0a6cd
Malek Mansour <138655005+MalekMansour@users.noreply.github.com>
2024-03-04 08:51:42 -0700

    Update login.cs

--- practice0/login.cs
+++ practice0/login.cs
@@ -1,1 +1,26 @@
+using System;
 
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Create a password: ");
+        string createPassword = Console.ReadLine();
+        string password = createPassword;
+
+        while (true)
+        {
+            Console.Write("Login with your password: ");
+            string login = Console.ReadLine();
+            if (login == password)
+            {
+                Console.WriteLine("Access Granted");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Access Denied, try again.");
+            }
+        }
+    }
+}
