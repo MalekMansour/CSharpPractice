5be5c8cfb9b8eb1170f43a1a049679be42721524
Malek Mansour <138655005+MalekMansour@users.noreply.github.com>
2024-03-05 16:41:56 -0700

    Update RAF.cs

--- practice2/RAF.cs
+++ practice2/RAF.cs
@@ -1,58 +1,8 @@
+# REDO
 using System;
+using System.Collections.Generic;
 using System.IO;
-using System.Text.Json;
-
-class Event
-{
-    public int EventNumber { get; set; }
-    public string Location { get; set; }
-}
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Event calgaryEvent = new Event
-        {
-            EventNumber = 1,
-            Location = "Calgary"
-        };
-
-        // serialization
-        string jsonString = JsonSerializer.Serialize(calgaryEvent);
-        File.WriteAllText("event.txt", jsonString);
-
-        // deserialization
-        string jsonFromFile = File.ReadAllText("event.txt");
-        Event deserializedEvent = JsonSerializer.Deserialize<Event>(jsonFromFile);
-        Console.WriteLine(deserializedEvent.EventNumber);
-        Console.WriteLine(deserializedEvent.Location);
-        Console.WriteLine("Tech Competition");
-        ReadFromFile();
-    }
-
-    static void ReadFromFile()
-    {
-        using (StreamWriter writer = new StreamWriter("event.txt"))
-        {
-            writer.Write("Hackathon");
-        }
-
-        // read first, middle, and last characters
-        using (StreamReader reader = new StreamReader("event.txt"))
-        {
-            string word = reader.ReadToEnd();
-            Console.WriteLine($"In Word: {word}");
-            int length = word.Length;
-
-            char firstChar = word[0];
-            char middleChar = word[length / 2];
-            char lastChar = word[length - 1];
-
-            Console.WriteLine($"The First Character is: \"{firstChar}\"");
-            Console.WriteLine($"The Middle Character is: \"{middleChar}\"");
-            Console.WriteLine($"The Last Character is: \"{lastChar}\"");
-            Console.ReadKey();
-        }
-    }
-}
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
