e1abd1a0faa773f42d1d0819cfa023cb5e682c9c
Malek Mansour <138655005+MalekMansour@users.noreply.github.com>
2024-03-05 14:07:38 -0700

    Update Factorial.cs

--- practice2/Factorial.cs
+++ practice2/Factorial.cs
@@ -1,1 +1,22 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a number to calculate its factorial:");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        long result = Factorial(num);
+        Console.WriteLine($"Factorial of {num} is: {result}");
+    }
+
+    static long Factorial(int n)
+    {
+        if (n == 0)
+            return 1;
+        else
+            return n * Factorial(n - 1);
+    }
+}
 
