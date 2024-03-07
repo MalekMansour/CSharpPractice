8dd687286f9277d778df203279f60af8f0c8b1b1
Malek Mansour <138655005+MalekMansour@users.noreply.github.com>
2024-03-07 12:24:31 -0700

    Update RAF.cs

--- practice2/RAF.cs
+++ practice2/RAF.cs
@@ -21,21 +21,45 @@ namespace lab6
             public Event() { }
         }
 
+        static void Main(string[] args)
+        {
+            try
+            {
+                Event calgaryEvent = new Event(1, "Calgary");
+
+                string filePath = "event.txt"; 
+
+                // serializing
+                SerializeEvent(filePath, calgaryEvent);
+
+                // deserializing 
+                Event deserializedEvent = DeserializeEvent(filePath);
+                Console.WriteLine(deserializedEvent.EventNumber);
+                Console.WriteLine(deserializedEvent.Location);
+
+                // reading from file 
+                ReadFromFile(filePath);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+            }
+            Console.ReadKey();
+        }
+
         // Method to serialize Event object to a file
         static void SerializeEvent(string filepath, Event eve)
         {
-            // Using BinaryFormatter to serialize the object
             using (FileStream stream = new FileStream(filepath, FileMode.Create))
             {
                 BinaryFormatter formatter = new BinaryFormatter();
                 formatter.Serialize(stream, eve);
             }
         }
-
+        
         // Method to deserialize Event object from a file
         static Event DeserializeEvent(string filepath)
         {
-            // Using BinaryFormatter to deserialize the object
             using (FileStream stream = new FileStream(filepath, FileMode.Open))
             {
                 BinaryFormatter formatter = new BinaryFormatter();
@@ -46,7 +70,6 @@ namespace lab6
         // Method to read from file and display first, middle, and last characters
         static void ReadFromFile(string filePath)
         {
-            // Writing "Hackathon" to the file
             using (StreamWriter writer = new StreamWriter(filePath))
             {
                 writer.Write("Hackathon");
@@ -55,7 +78,6 @@ namespace lab6
             Console.WriteLine("Tech Competition");
             Console.WriteLine("In Word: Hackathon");
 
-            // Reading first, middle, and last characters using FileStream and Seek method
             using (FileStream fileStream = new FileStream(filePath, FileMode.Open))
             {
                 byte[] buffer = new byte[3];
@@ -75,35 +97,6 @@ namespace lab6
                 fileStream.Read(buffer, 0, 1);
                 Console.WriteLine($"The Last Character is: \"{(char)buffer[0]}\"");
             }
-        }
-
-        static void Main(string[] args)
-        {
-            try
-            {
-                // Creating an Event object
-                Event calgaryEvent = new Event(1, "Calgary");
-
-                string filePath = "event.txt"; // File name changed as per instruction
-
-                // Serializing the Event object
-                SerializeEvent(filePath, calgaryEvent);
-
-                // Deserializing the Event object and displaying values
-                Event deserializedEvent = DeserializeEvent(filePath);
-                Console.WriteLine(deserializedEvent.EventNumber);
-                Console.WriteLine(deserializedEvent.Location);
-
-                // Reading from file and displaying characters
-                ReadFromFile(filePath);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("An error occurred: " + ex.Message);
-            }
-
-            // Waiting for user input to close the console
-            Console.ReadKey();
         }
     }
 }
